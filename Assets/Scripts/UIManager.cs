2926f80f104c70830d8af9c2414dba3ebc3a4c46
Dennisse M Pagan Davila <dennisse.freelancedevelopment@gmail.com>
2021-04-30 22:30:02 -0400

    Added Flickering Game Over Script

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,26 +9,53 @@ public class UIManager : MonoBehaviour
     private Text _scoreText;
 
     [SerializeField]
+    private Text _gameOverText;
+
+    [SerializeField]
     private Sprite[] _livesSprites;
 
     [SerializeField]
     private Image _livesImg;
-  
+
 
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
-   public void updateScore(int playerScore)
+    public void updateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore.ToString();
     }
     public void updateLives(int currentLives)
     {
         _livesImg.sprite = _livesSprites[currentLives];
- 
+
+        if (currentLives == 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlicker());
+        }
+        IEnumerator GameOverFlicker()
+        {
+            while (currentLives == 0)
+                {
+                _gameOverText.enabled = false;
+
+                yield return new WaitForSeconds(0.5f);
+                _gameOverText.enabled = true;
+                yield return new WaitForSeconds(0.5f);
+            }
+            
+        }
     }
+
 }
+   
+       
+
+
+
